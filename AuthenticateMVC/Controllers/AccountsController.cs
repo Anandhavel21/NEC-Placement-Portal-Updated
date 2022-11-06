185891f341d1cd8b34120b3ed0ba2403f7f0b5ad
DELL <DELL@DESKTOP-UHACNIF>
2022-11-06 21:28:54 +0530

    Add project files.

---
+++ AuthenticateMVC/Controllers/AccountsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using AuthenticateMVC.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.Globalization;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace AuthenticateMVC.Controllers
+{
+    public class AccountsController : Controller
+    {
+        TACV_DBEntities entity = new TACV_DBEntities();
+
+        // GET: Accounts
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel Credentials)
+        {
+            bool userexit = entity.UsersTbls.Any(x => x.Email == Credentials.Email && x.Passcode == Credentials.Password);
+            UsersTbl u = entity.UsersTbls.FirstOrDefault(x => x.Email == Credentials.Email && x.Passcode == Credentials.Password);
+            if (userexit)
+            {
+                FormsAuthentication.SetAuthCookie(u.Username,false);
+                return RedirectToAction("Index","Home");
+            }
+
+            //ModelState.AddModelError("", "Username or Password is wrong");
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Signup(UsersTbl userinfo)
+        {
+            entity.UsersTbls.Add(userinfo);
+            entity.SaveChanges();
+            return RedirectToAction("Login");
+        }
+
+        public ActionResult Signout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        //public async Task<ActionResult> ForgotPassword(ForgetViewModel model)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        //var user = await UserManagerExtensions.FindByEmail(model.Email);
+        //        var user = await UserManager
+
+                
+
+        //        return View();
+        //    }
+        //    return View(model);
+        //}
+
+        public ActionResult CheckMail()
+        {
+            return View();
+        }
+    }
+}
---
+++ AuthenticateMVC/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticateMVC.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Enter your E-Mail ID!!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter the Password!!"),StringLength(16,MinimumLength =6)]
+        public string Password { get; set; }
+    }
+}
