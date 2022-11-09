706f466e185ad6c4b73c863d146130b4b21cd1ee
Anandhavel21 <1910002@nec.edu.in>
2022-11-09 18:47:13 +0530

    Add project files.

---
+++ AuthenticateMVC/Models/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AuthenticateMVC.Models
+{
+    public class Class1
+    {
+
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public int Academicyear { get; set; }
+        public int Semester { get; set; }
+        public float SSLC { get; set; }
+        public float HSLC { get; set; }
+        public float CGPA { get; set; }
+    }
+}
